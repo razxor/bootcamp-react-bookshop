75653d624a6301116c98490c4a2e2b160d5d5e94
Raz Ahamed <raz.abcoder@gmail.com>
2024-09-23 13:18:53 +0600

    bug fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,12 @@ export const Hero = () => {
   return (
     <div className="grid sm:grid-cols-2 md:grid-cols-2 3xl:h-screen bg-gray-50 shadow-sm">
             <div className="col-span-1 flex flex-col justify-center px-16 py-6 sm:py-6">                
-                <div className='text-5xl md:text-4xl lg:text-7xl font-bold'>Unlock a World of  Stories</div>                
+                <div className='text-4xl sm:text-4xl md:text-4xl lg:text-7xl font-bold'>Unlock a World of  Stories</div>                
                 <p className='py-2'>
                 Find Your Next Favorite Book at Book Online Shop
                 </p>
                 <div className='flex gap-5 mt-6 lg:mb-1'>
-                    <button className='bg-orange-500 px-6 w-52 hover:bg-lime-500 py-4 rounded text-white text-lg font-bold' >
+                    <button className='bg-orange-500 px-6 w-52 sm:w-full md:w-52 hover:bg-lime-500 py-4 rounded text-white text-lg font-bold' >
                     <i className="fa-solid fa-cart-shopping"></i> Buy Book
                     </button>                    
                 </div>
