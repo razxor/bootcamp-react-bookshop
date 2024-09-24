6e665139e6eed7e8405790b29d142d042437d89b
Raz Ahamed <raz.abcoder@gmail.com>
2024-09-24 13:21:19 +0600

    dynamic page title added

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-helmet-async']
-  }
+  // optimizeDeps: {
+  //   include: ['react-helmet-async']
+  // }
+  resolve: {
+    alias: {
+      // Add this to avoid case-sensitive issues or missing deps
+      'react-helmet-async': 'react-helmet-async',
+    },
+  },
 })
